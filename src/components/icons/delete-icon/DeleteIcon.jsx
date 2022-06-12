a51ad9e2fb218e2b0ddc25c8d04e21c670c87c40
airman5573 <rpf5573@kakao.com>
2022-06-12 15:11:15 +0900

    refactor(component): props의 타입을 엄밀하게 검증한다

--- src/components/icons/delete-icon/DeleteIcon.jsx
+++ src/components/icons/delete-icon/DeleteIcon.jsx
@@ -4,10 +4,11 @@ import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import styles from "./delete-icon.module";
 
 function DeleteIcon({ onClick, className }) {
+  if (onClick && onClick !== 'function') throw new Error("onClick은 함수만 가능합니다");
   return (
     <button
       type="button"
-      onClick={() => onClick && onClick()}
+      onClick={onClick}
       className={cn(styles.deleteIcon, className)}
     >
       <FontAwesomeIcon icon={faTrashCan} />
