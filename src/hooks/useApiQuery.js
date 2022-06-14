5281d0c74add002754495ac1c77b245f7118c2b4
airman5573 <rpf5573@kakao.com>
2022-06-14 16:56:29 +0900

    refactor(hook): reducer를 활용해서 상태관리를 개선한다
    useState를 여러번 사용하는 대신, useReducer를 활용한다.

--- src/hooks/useApiQuery.js
+++ src/hooks/useApiQuery.js
@@ -1,27 +1,64 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+
+const ACTIONS = {
+  LOADING: "LOADING",
+  FULLFILLED: "FULLFILLED",
+  REJECTED: "REJECTED",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.LOADING: {
+      const newState = structuredClone(state);
+      newState.isLoading = true;
+      newState.error = null;
+      return newState;
+    }
+    case ACTIONS.FULLFILLED: {
+      const newState = structuredClone(state);
+      newState.isLoading = false;
+      newState.error = null;
+      newState.data = payload;
+      return newState;
+    }
+    case ACTIONS.REJECTED: {
+      const newState = structuredClone(state);
+      newState.isLoading = false;
+      newState.error = payload;
+      return newState;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+const initialState = {
+  isLoading: false,
+  error: null,
+  data: null,
+};
 
 function useApiQuery({ queryFn, queryOnMount = false, onSuccess, onError }) {
   if (typeof queryFn !== "function") {
     throw new Error("queryFn는 함수이어야 합니다");
   }
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
+  const [{ isLoading, error, data }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const queryWithStateControl = async () => {
-    setError(null);
-    setIsLoading(true);
-
+    dispatch({ type: ACTIONS.LOADING });
     try {
       const response = await queryFn();
       const data = await response.json();
-      setData(data);
+      dispatch({ type: ACTIONS.FULLFILLED, payload: data });
       if (onSuccess && typeof onSuccess === "function") {
         onSuccess(data);
       }
     } catch (e) {
-      setIsLoading(false);
-      setError(e);
+      dispatch({ type: ACTIONS.REJECTED, payload: e })
       if (onError && onError === "function") {
         onError(e);
       }
